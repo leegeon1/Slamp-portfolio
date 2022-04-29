dd2ad0a272da1f96e391178492e260afb89763f3
leegeon1 <dlrjs1509@naver.com>
2022-04-29 18:00:18 +0900

    0429

--- kr.co.dothome.slamp portfolio/html/js/slamp_sub1.js
+++ kr.co.dothome.slamp portfolio/html/js/slamp_sub1.js
@@ -1,17 +1,60 @@
 $(function(){
     var i = 0;
-    var btn = $('.banner.bnr01 .icon');
+    var btn1 = $('.banner.bnr01 .icon');
+    var icimg = $('.banner.bnr01 .icon .image');
     var b2 = $('.banner.bnr02');
+    var art1 = $('.content.cnt01 section .artGroup');
+    var btn2 = $('.content.cnt01 section .btn');
 
 
-    btn.css({
-        filter: 'grayscale(0)'
-    });
-    btn.click(function(){
+    
+    
+    
+    
+    
+    
+    // 파라미터
+    btn1.click(bnr1);
+
+    btn2.click(cnt1);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // 배너1
+    function bnr1(){
         var ind = $(this).index();
         b2.find('.bnrTeam').css({
             marginLeft : ind * -100+'%',
         });
-        
-    }); 
+    }; 
+    
+    // 컨텐츠1
+    art1.css({
+        marginLeft : '-33.3%'
+    });
+    function cnt1(){
+        i++;
+        if(i>=5){
+            i=5;
+        }
+        art1.animate({
+            marginLeft : '0%',
+        },500,function(){
+            art1.find('.article:first').appendTo(art1);
+            art1.css({marginLeft : '-33.3%'});
+        });
+        $('.article').css('filter', 'grayscale(1)');
+        $('.article').eq(i).css('filter', 'grayscale(0)');
+    }
 });
